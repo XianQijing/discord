cb07bcb478a4b311a51a98e5089493a2d7c2797e
XianQijing <154122662@qq.com>
2025-05-30 12:08:31 +0800

    增加入参校验

--- routes/query.js
+++ routes/query.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const PackageService = require('../services/packageService')
+const { AppError } = require('../middleware/errorHandler');
 
 // 获取所有package数据
 router.get('/', async (req, res, next) => {
   const { id } = req.query;
   try {
+    if (id && !/^[a-zA-Z0-9]+$/.test(id)) {
+      throw new AppError('Id只能包含字母、数字', 400);
+    }
     const [list] = await PackageService.getList(id)
 
     res.json(list);
